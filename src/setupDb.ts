18dbfcba311520ef329f8cf1a867b0881dbac121
PBM42 <pbm764927@gmail.com>
2019-02-07 16:59:50 +0100

    refactor: setupDb

--- src/setupDb.ts
+++ src/setupDb.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, Db, Cursor } from 'mongodb';
 import { v1 as Neo } from 'neo4j-driver';
 
 interface OFFProduct {
@@ -17,12 +17,72 @@ enum Preservation {
 }
 
 const main = async () => {
-  const driver = Neo.driver('bolt://localhost', Neo.auth.basic('neo4j', 'admin'));
-  const session = driver.session();
   const mongoClient = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: true });
   await mongoClient.connect();
   const db = mongoClient.db('off');
-  const cursor = db.collection('products').find(
+  const cursor = retrieveProducts(db);
+  await handleProducts(cursor);
+  mongoClient.close();
+};
+
+const handleProducts = async (cursor: Cursor<OFFProduct>) => {
+  const driver = Neo.driver('bolt://localhost', Neo.auth.basic('neo4j', 'admin'));
+  const session = driver.session();
+
+  while (cursor.hasNext) {
+    const obj: OFFProduct = (await cursor.next()) as OFFProduct;
+    const stores: string[] = obj.stores.replace(', ', ',').split(',');
+    const countries: string[] = obj.countries_tags.map(tag => tag.slice(3));
+    if (countries.length === 0) {
+      countries.push('');
+    }
+    const storeAndRelations = buildStoresAndRelations(stores, countries);
+    const toRun = `
+        ${buildProduct}
+        ${storeAndRelations}
+      `;
+    await session.run(toRun);
+  }
+  session.close();
+  driver.close();
+};
+const buildProduct = (obj: OFFProduct) => {
+  const preservation = buildPreservation(obj);
+  return `MERGE (p:Product {
+    code: "${obj.code}",
+    name: "${sanitizeProductName(obj.product_name)}"
+    ${preservation}
+  })`;
+};
+
+const buildPreservation = (obj: OFFProduct) =>
+  obj.packaging_tags
+    ? ', preservation: ' +
+      JSON.stringify(
+        obj.packaging_tags.map(packagingTagToPreservation).filter(p => p !== Preservation.none)
+      )
+    : '';
+
+const buildStoresAndRelations = (stores: string[], countries: string[]) => {
+  const productRelations: string[] = [];
+  const storeCreation = stores.reduce((acc, storeName, sI) => {
+    const newStores = countries
+      .map((country, cI) => {
+        productRelations.push(`MERGE (s${sI + '_' + cI})-[:SELL]->(p)`);
+        return `MERGE (s${sI + '_' + cI}:Store {
+            name: "${storeName.toLowerCase()}",
+            country: "${country}"
+          })
+        `;
+      })
+      .join('\n');
+    return `${acc}${newStores}`;
+  }, '');
+  return storeCreation + productRelations.join('\n');
+};
+
+const retrieveProducts = (db: Db) =>
+  db.collection('products').find(
     {
       product_name: { $exists: true, $ne: '' },
       stores: { $exists: true, $ne: '' },
@@ -41,44 +101,6 @@ const main = async () => {
       },
     }
   );
-  while (cursor.hasNext) {
-    const obj: OFFProduct = await cursor.next();
-    const stores: string[] = obj.stores.replace(', ', ',').split(',');
-    const countries: string[] = obj.countries_tags.map(tag => tag.slice(3));
-    if (countries.length === 0) {
-      countries.push('');
-    }
-    const productRelations: string[] = [];
-    const storeCreation = stores.reduce((acc, storeName, sI) => {
-      const newStores = countries
-        .map((country, cI) => {
-          productRelations.push(`MERGE (s${sI + '_' + cI})-[:SELL]->(p)`);
-          return `MERGE (s${sI +
-            '_' +
-            cI}:Store { name: "${storeName.toLowerCase()}", country: "${country}"})
-        `;
-        })
-        .join('\n');
-      return `${acc}${newStores}`;
-    }, '');
-    const preservation = obj.packaging_tags
-      ? ', preservation: ' +
-        JSON.stringify(
-          obj.packaging_tags.map(packagingTagToPreservation).filter(p => p !== Preservation.none)
-        )
-      : '';
-    const toRun = `
-        MERGE (p:Product { code: "${obj.code}", name: "${sanitizeProductName(
-      obj.product_name
-    )}"${preservation} })
-        ${storeCreation}${productRelations.join('\n')}
-      `;
-    await session.run(toRun);
-  }
-  mongoClient.close();
-  session.close();
-  driver.close();
-};
 
 const packagingTagToPreservation = (tag: string): Preservation => {
   switch (tag) {
